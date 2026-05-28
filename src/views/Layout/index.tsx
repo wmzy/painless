670dceb26fbec73e83d6c3dcab550c6bec30336a
wmzy <1256573276@qq.com>
2026-05-28 16:59:16 +0800

    feat: rewrite Layout with haze-ui NavigationBar and Container

--- src/views/Layout/index.tsx
+++ src/views/Layout/index.tsx
@@ -1,52 +1,24 @@
 import {css} from '@linaria/core';
-import {Link, PrefetchLink, View} from '@native-router/react';
+import {View} from '@native-router/react';
+import {NavigationBar, NavLink, Container, Title} from 'haze-ui';
 
 export default function Layout() {
   return (
-    <section className={css``}>
-      <header
-        x-class={css`
-          border-bottom: 1px dashed;
-          display: flex;
-          justify-content: space-between;
-          align-items: center;
-          padding: 0 24px;
-
-          > nav > ul {
-            display: flex;
-            list-style: none;
-            gap: 16px;
-          }
-        `}
-      >
-        <h1>
-          <PrefetchLink
-            x-class={css`
-              color: #5cb85c;
-            `}
-            to='/'
-          >
-            Painless
-          </PrefetchLink>
-        </h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Link to='/help'>Help</Link>
-            </li>
-            <li>
-              <Link to='/about'>About</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>
+    <div>
+      <NavigationBar>
+        <NavLink to='/'>
+          <Title level={3}>Painless</Title>
+        </NavLink>
+        <NavLink to='/'>Home</NavLink>
+        <NavLink to='/help'>Help</NavLink>
+        <NavLink to='/about'>About</NavLink>
+        <NavLink to='/login'>Login</NavLink>
+        <NavLink to='/register'>Register</NavLink>
+      </NavigationBar>
+      <Container>
         <View />
-      </main>
-    </section>
+      </Container>
+    </div>
   );
 }
 
